edab3180c5f4f5d11e40d683144a816542058a99
salman khan <cloudchamp0325@gmail.com>
2026-01-08 00:41:54 +0500

    removing one line of code for the test

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -16,7 +16,7 @@ function Layout({ children }) {
           </div>
         </div>
       </header>
-      <AnimatedBanner message="Welcome to DevOps Shack 🚀" />
+      <AnimatedBanner message="Welcome to DevOps 🚀" />
       <div className="app-body">
         <aside className="sidebar slide-in-left">
           <h3>Connect</h3>
